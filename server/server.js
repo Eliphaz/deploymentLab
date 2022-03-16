f039b26f227e0e67b6f16f0d5f49821246fe5b99
Eliphaz <eliahlander3@gmail.com>
2022-03-16 14:44:14 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT || 5500
